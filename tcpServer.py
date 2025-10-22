15779f06d092a7f089256e5e2b5a612e67f73ea2
Denton <163344926+cDenton1@users.noreply.github.com>
2025-10-22 15:51:30 -0600

    Add files via upload

---
+++ tcpServer.py
@@ -0,0 +1,17 @@
+import socket
+
+serverPort = 12000      # chosen port
+serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+serverSocket.bind(('',serverPort))  # binds socket to host and port
+serverSocket.listen(1)              # listens for incoming connection
+
+print('The server is ready to receive')
+while True:
+    connectionSocket, addr = serverSocket.accept()  # accepts connection
+    sentence = connectionSocket.recv(1024).decode() # receives data and decodes it
+    
+    capitalizedSentence = sentence.upper()              # capitalizes data
+    connectionSocket.send(capitalizedSentence.encode()) # encodes and sends back
+    
+    connectionSocket.close()        # closes socket
---
+++ udpServer.py
@@ -0,0 +1,13 @@
+import socket
+
+serverPort = 12000      # chosen port
+serverSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+serverSocket.bind(('', serverPort))     # binds socket to host and port
+
+print('The server is ready to receive')
+while True:
+    message, clientAddress = serverSocket.recvfrom(2048)    # receives data
+    modifiedMessage = message.decode().upper()              # decodes and capitalizes data
+    
+    serverSocket.sendto(modifiedMessage.encode(), clientAddress)    # encodes and sends back
