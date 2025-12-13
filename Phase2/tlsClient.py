65fc66698c7c6591ee47a4a5f1e8f4c44fb30b91
Denton <chloedenton2005@gmail.com>
2025-12-12 23:12:57 -0700

    added comments to code

--- Phase2/tlsClient.py
+++ Phase2/tlsClient.py
@@ -1,33 +1,33 @@
 import socket
 import ssl
 
-serverName = '10.0.2.15'
-serverPort = 12000
+serverName = '10.0.2.15'    # server IP address
+serverPort = 12000          # chosen port 
 
-def main():
-    context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
+def main():                 # main function, makes future modularity easier
+    context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)                           # SSL context for TLS client
 
-    context.load_verify_locations("server.crt")
-    context.verify_mode = ssl.CERT_REQUIRED
-    context.check_hostname = True
+    context.load_verify_locations("ca.crt")                                     # trusted certificate is loaded
+    context.verify_mode = ssl.CERT_REQUIRED                                     # requires server certificate verification
+    context.check_hostname = True                                               # requires hostname verification
 
-    clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    tlsSocket = context.wrap_socket(clientSocket, server_hostname=serverName)
+    clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)            # creates TCP socket
+    tlsSocket = context.wrap_socket(clientSocket, server_hostname=serverName)   # wraps TCP socket in TLS
 
     try:
-        tlsSocket.connect((serverName, serverPort))
+        tlsSocket.connect((serverName, serverPort))             # connects to server
         
-        sentence = input("Input lowercase sentence: ")
-        tlsSocket.send(sentence.encode())
+        sentence = input("Input lowercase sentence: ")          # stores user input
+        tlsSocket.send(sentence.encode())                       # encodes and sends data
 
-        modifiedSentence = tlsSocket.recv(1024).decode()
-        print("From Server:", modifiedSentence)
+        modifiedSentence = tlsSocket.recv(1024).decode()        # receives data, max 1024 bytes, and decodes it
+        print("From Server:", modifiedSentence)                 # prints received data
 
-    except ssl.SSLError as e:
-        print(f"TLS certification verification failed: {e}")
+    except ssl.SSLError as e:                                   # catches TLS/SSL errors
+        print(f"TLS certification verification failed: {e}")    # displays error
 
     finally:
-        tlsSocket.close()
+        tlsSocket.close()   # closes socket
 
 if __name__ == "__main__":
     main()
--- Phase2/tlsServer.py
+++ Phase2/tlsServer.py
@@ -1,35 +1,34 @@
 import socket
 import ssl
 
-serverPort = 12000
+serverPort = 12000      # chosen port
 
-def main():
-    context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
-    context.load_cert_chain(certfile="server.crt", keyfile="server.key")
+def main():             # main function, makes future modularity easier
+    context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)                       # SSL context for TLS server
+    context.load_cert_chain(certfile="server.crt", keyfile="server.key")    # server certificate and private key are loaded
 
-    serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    serverSocket.bind(('', serverPort))
-    serverSocket.listen(1)
+    serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)        # creates TCP socket
+    serverSocket.bind(('', serverPort))                                     # binds socket to host and port
+    serverSocket.listen(1)                                                  # listens for incoming connection
 
     print("The TLS server is ready to receive")
     while True:
-        tcpConnection, addr = serverSocket.accept()
+        tcpConnection, addr = serverSocket.accept()                                 # accepts connection
         print(f"Connection from {addr}, attempting TLS")
-
-        tlsConnection = context.wrap_socket(tcpConnection, server_side=True)
+        tlsConnection = context.wrap_socket(tcpConnection, server_side=True)        # wraps connection in TLS
 
         try:
-            sentence = tlsConnection.recv(1024).decode()
+            sentence = tlsConnection.recv(1024).decode()                            # receives data, max 1024 bytes, and decodes it
 
-            capSentence = sentence.upper()
+            capSentence = sentence.upper()                                          # capitalizes data
             print(f"Data recieved: {sentence} -> Data returning: {capSentence}")
-            tlsConnection.send(capSentence.encode())
+            tlsConnection.send(capSentence.encode())                                # encodes and sends back
         
-        except Exception as e:
-            print(f"TLS Error: {e}")
+        except Exception as e:          # catches exceptions
+            print(f"TLS Error: {e}")    # displays
         
         finally:
-            tlsConnection.close()
+            tlsConnection.close()       # close socket
         
 if __name__ == "__main__":
     main()
